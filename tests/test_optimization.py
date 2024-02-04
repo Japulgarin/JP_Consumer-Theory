6fa9175ccee3a0845ad5baa9688a8380070450f1
Jorge <japulgarin@uninorte.edu.co>
2024-02-03 21:53:20 -0500

    Test

---
+++ tests/test_optimization.py
@@ -0,0 +1,11 @@
+import unittest
+from jp_consumer_theory.optimization import utility_optimize
+
+class TestOptimization(unittest.TestCase):
+    def test_utility_optimize(self):
+        # Here you will call utility_optimize with some parameters
+        # And assert expected outcomes
+        self.assertEqual(True, True)  # Placeholder assertion
+
+if __name__ == '__main__':
+    unittest.main()
